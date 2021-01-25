8e3ae6652acdd982c6b46af9e1d0d7bb9edcfbcc
黄飞鹏 <1094521084@qq.com>
2021-01-25 18:14:50 +0800

    加个点击群名称修改保存功能，加了点击设置，可以邀请成员，退出群组，解散群组，升降管理员，踢人，禁言功能目前还在做。。。

--- src/components/chat_frame/drawer/group_detail/group_detail.vue
+++ src/components/chat_frame/drawer/group_detail/group_detail.vue
@@ -5,12 +5,35 @@
       <vue-scroll :ops="ops" ref="vs">
         <h3>群组信息</h3>
         <div class="group_title">
-          <p class="groupName" v-if="data.name && data.affiliations_count">
-            {{ data.name }}{{ `(${data.affiliations_count})` }}
+          <p
+            class="groupName"
+            v-show="data.name && data.affiliations_count"
+            :contenteditable="!allowChangeGroupName"
+            @input="setGroupName"
+            ref="groupName"
+          >
+            {{ data.name }}
+            <span v-show="allowChangeGroupName">{{
+              `(${data.affiliations_count})`
+            }}</span>
           </p>
+
           <!-- 修改群组信息 -->
           <span class="groupChangeInfo_btn" v-if="roleNum.num == 0">
-            <img src="@/assets/image/修改.png" title="修改群组信息" />
+            <img
+              class="amend"
+              @click="setGroupName"
+              v-if="allowChangeGroupName"
+              :src="amend.img"
+              :title="amend.title"
+            />
+            <img
+              class="save"
+              v-else
+              @click="saveGroupName"
+              :src="save.img"
+              :title="save.title"
+            />
           </span>
           <br />
           <!-- <span class="groupId">{{ data.id }}</span> -->
@@ -59,10 +82,32 @@
                 class="groupMem_setBtn"
                 v-if="roleNum.num == 0 && item.member"
               >
-                <div :class="[ adminList.includes(item.member)?'notAdmin':'isAdmin']"></div>
-                <div :class="[ allMuteList.includes(item.member)?'notMuteMem':'isMuteMem']"></div>
+                <!-- 升降管理员 -->
+                <div
+                  :class="[
+                    adminList.includes(item.member) ? 'notAdmin' : 'isAdmin'
+                  ]"
+                  @click="setAdmin(item.member)"
+                ></div>
+                <!-- 禁言成员 -->
+                <div
+                  :class="[
+                    allMuteList.includes(item.member)
+                      ? 'notMuteMem'
+                      : 'isMuteMem'
+                  ]"
+                  @click="setMuteMem(item.member)"
+                ></div>
+                <!-- 移出成员 -->
                 <div class="delMem">
-                  <span class="iconfont icon-exittuichu"></span>
+                  <span
+                    class="iconfont icon-exittuichu"
+                    @click="
+                      isModal = true
+                      modalType = 3
+                      nowPickMember = item.member
+                    "
+                  ></span>
                 </div>
               </div>
             </li>
@@ -70,28 +115,114 @@
         </div>
       </vue-scroll>
     </div>
+
+    <!-- 群组设置 -->
+    <div class="set_Btn">
+      <span
+        class="iconfont icon-ziyuan156"
+        @click.stop="isShowGroup_set = !isShowGroup_set"
+      ></span>
+    </div>
+    <transition name="slide-fade">
+      <div class="group_set" v-show="isShowGroup_set">
+        <!-- 群主或者管理员方可做出邀请 -->
+        <span
+          @click="
+            isModal = true
+            modalType = 0
+          "
+          v-if="roleNum.num == 0 || roleNum.num == 1"
+          >邀请成员</span
+        >
+        <!-- 管理员或者群成员方可退出 -->
+        <span
+          @click="
+            isModal = true
+            modalType = 2
+          "
+          v-if="roleNum.num == 2 || roleNum.num == 1"
+          >退出群组</span
+        >
+        <!-- 只有群主方可解散群组 -->
+        <span
+          @click="
+            isModal = true
+            modalType = 1
+          "
+          v-if="roleNum.num == 0"
+          >解散群组</span
+        >
+      </div>
+    </transition>
+
+    <!-- 弹出模态框 -->
+    <Modal :modalTitle="modalTitle" v-if="isModal" class="groupMadal">
+      <div slot="modal-content" class="GroupText">
+        <div v-if="modalType == 0">
+          <h3>好友名称：</h3>
+          <input
+            type="text"
+            class="memId"
+            v-model="memId"
+            placeholder="邀请人ID..."
+          />
+        </div>
+        <div v-if="modalType == 1">
+          <h3 style="color:red">您确定要解散该群？三思啊！</h3>
+        </div>
+        <div v-if="modalType == 2">
+          <h3>您确定要退出该群？</h3>
+        </div>
+        <div v-if="modalType == 3">
+          <h3>您确定要移除该成员？</h3>
+        </div>
+      </div>
+      <div slot="modal-footer" class="btn-box" v-if="modalType == 0">
+        <button class="not_btn" @click="isModal = false">取消</button>
+        <button class="yes_btn" @click="inviteMem">邀请</button>
+      </div>
+      <div slot="modal-footer" class="btn-box" v-if="modalType == 1">
+        <button class="not_btn" @click="isModal = false">取消</button>
+        <button class="yes_btn" @click="dissolveGroup">解散</button>
+      </div>
+      <div slot="modal-footer" class="btn-box" v-if="modalType == 2">
+        <button class="not_btn" @click="isModal = false">取消</button>
+        <button class="yes_btn" @click="quitGroup">确定</button>
+      </div>
+      <div slot="modal-footer" class="btn-box" v-if="modalType == 3">
+        <button class="not_btn" @click="isModal = false">取消</button>
+        <button class="yes_btn" @click="removeMember">确定</button>
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
 import "./group_detail.scss"
-import { mapState } from "vuex"
+import { mapState, mapActions } from "vuex"
 import Ops from "@/utils/scrollConfig"
+import Modal from "@/components/modal-box/modal-box"
 export default {
   data() {
     return {
       ops: Ops,
-      data: [],
-      nowLoginId: this.$conn.user,
-      announcement: "",
+      modalTitle: "群组操作",
+      isModal: false,
+      memId: "",
+      modalType: 0, //0邀请成员、1解散群组、2退出群组、3移出群组
+      nowPickMember: "", //当前选中的群成员、
+      data: [], //群组详情
+      nowLoginId: this.$conn.user, //当前登录人ID
+      announcement: "", //群公告
       groupOwner: "", //该群群主
       adminList: [], //管理员列表
-      allMuteList: [],
-      allBlackList: [],
+      allMuteList: [], //群组禁言列表
+      allBlackList: [], //TO DO 群黑名单列表，但未使用
       roleNum: {
         num: null
       }, //role 0 群主 1管理员 2普通成员
       //permissions 权限管理，role 0 群主 1管理员 2普通成员
+      /* permissions功能待定 */
       permissions: {
         0: {
           modifyGroup: true, //修改群组信息
@@ -123,7 +254,12 @@ export default {
           mute_btn: false, //禁言功能,
           blackList_btn: false //黑名单功能
         }
-      }
+      },
+      allowChangeGroupName: true, //控制是否容许修改群名称
+      newGroupName: "", //新群名
+      amend: { img: require("@/assets/image/修改.png"), title: "点击修改群名" },
+      save: { img: require("@/assets/image/上传.png"), title: "点击保存修改" },
+      isShowGroup_set: false //控制群设置的显隐
     }
   },
   async created() {
@@ -144,6 +280,7 @@ export default {
     }
   },
   methods: {
+    ...mapActions(["getUserName", "getGroupsList"]),
     setRole(param) {
       //处理当前角色权限
       let nowLoginId = this.$conn.user
@@ -157,6 +294,41 @@ export default {
       ) {
         this.$set(this.roleNum, "num", 2)
       }
+    },
+    setGroupName(e) {
+      this.allowChangeGroupName = false
+      this.$nextTick(() => {
+        this.newGroupName = this.$refs["groupName"].innerText
+        // let title = this.$refs["groupName"].innerText
+        console.log(this.newGroupName)
+
+        this.$refs["groupName"].style.border = "2px dashed #1052c2"
+        this.$refs["groupName"].style.borderRadius = "5px"
+      })
+    },
+    saveGroupName() {
+      //保存群名的修改,后期再加修改群详情
+      this.allowChangeGroupName = true
+      this.$nextTick(() => {
+        this.$refs["groupName"].style.border = "none"
+        this.$refs["groupName"].style.borderRadius = "none"
+        let option = {
+          groupId: this.groupId,
+          groupName: this.newGroupName, // 群组名称
+          description: "Change group information test" // 群组简介
+        }
+        if (this.data.name !== this.newGroupName) {
+          this.$conn.modifyGroup(option).then(res => {
+            // TO DO 投机取巧直接调用了再拉一遍群列表，理想是直接搜索查找store里存的值然后修改。
+            this.getGroupsList(),
+              this.getUserName({
+                chatID: this.groupId,
+                chatName: this.newGroupName,
+                type: 1
+              })
+          })
+        }
+      })
     },
     async getGroupInfo() {
       let options = {
@@ -204,7 +376,143 @@ export default {
       this.$conn.getGroupBlacklistNew(option).then(res => {
         this.allBlackList = res.data
       })
+    },
+    inviteMem() {
+      let option = {
+        users: [this.memId],
+        groupId: this.groupId
+      }
+      this.memId &&
+        this.$conn
+          .inviteToGroup(option)
+          .then(res => {
+            res.data[0].action == "invite" &&
+              this.$notify({
+                title: `邀请成功`,
+                message: `您邀请的信件已发出,静候佳音吧！`,
+                type: "success"
+              })
+          })
+          .catch(e => {
+            console.log(e.data)
+            let errorType = JSON.parse(e.data)
+            if (errorType.error == "forbidden_op") {
+              let error = errorType.error_description.split(",")
+              //操作目的是为了查询详情中是否存在’already‘这个单词，如果有就代表该成员已经存在。
+              if (error[1].split(" ").includes("already")) {
+                this.$notify.error({
+                  title: `邀请失败`,
+                  message: `${error[1].split(" ")[2]}已经存在于群组${
+                    error[1].split(" ")[6]
+                  }当中。`
+                })
+              }
+            } else if (errorType.error == "resource_not_found") {
+              let error = errorType.error_description.split(" ")
+              if (error.includes("doesn't") && error.includes("exist!")) {
+                this.$notify.error({
+                  title: `邀请失败`,
+                  message: `"${error[1]}"这人压根就不存在,你再好好看看！`
+                })
+              }
+            }
+          })
+      this.memId = ""
+    },
+    quitGroup() {
+      // 成员主动退出群
+      let option = {
+        groupId: this.groupId
+      }
+      this.$conn.quitGroup(option).then(res => {
+        this.$notify({
+          title: `退出成功`,
+          message: `已退出"${this.data.name}"群~`,
+          type: "success"
+        })
+        this.getGroupsList(),
+          this.getUserName({
+            chatID: "",
+            chatName: "",
+            type: 1
+          })
+      })
+    },
+    dissolveGroup() {
+      // 解散一个群组
+      let option = {
+        groupId: this.groupId
+      }
+      this.$conn.dissolveGroup(option).then(res => {
+        console.log(res)
+        this.$notify({
+          title: `解散成功`,
+          message: `该群已解散`,
+          type: "success"
+        })
+        this.getGroupsList(),
+          this.getUserName({
+            chatID: "",
+            chatName: "",
+            type: 1
+          })
+      })
+    },
+    removeMember() {
+      // 移除群组成员
+      let option = {
+        groupId: this.groupId,
+        username: this.nowPickMember // 群组成员名称
+      }
+      this.nowPickMember &&
+        this.$conn.removeSingleGroupMember(option).then(res => {
+          this.$notify({
+            title: `移出成功`,
+            message: `已将"${this.nowPickMember}"移出本群~`,
+            type: "success"
+          })
+          this.nowPickMember = ""
+          this.getGroupInfo()
+          this.isModal = false
+        })
+    },
+    setAdmin(mem) {
+      let options = {
+        groupId: this.groupId, // 群组id
+        username: mem // 用户名
+      }
+      if (mem && !this.adminList.includes(mem)) {
+        this.$conn.setAdmin(options).then(res => {
+          console.log(">>>>>设置成功", res)
+          this.getGroupAdmin()
+        })
+      } else {
+        this.$conn.removeAdmin(options).then(res => {
+          console.log(">>>>>>>>移出成功", res)
+          this.getGroupAdmin()
+        })
+      }
+    },
+    setMuteMem(mem) {
+      let option = {
+        username: mem, // 成员用户名
+        muteDuration: -1, // 禁言的时长，单位是毫秒
+        groupId: this.groupId
+      }
+      if (mem && !this.allMuteList.includes(mem)) {
+        this.$conn.mute(options).then(res => {
+          console.log('>>>>>>禁言成功',res)
+        })
+      } else {
+        this.$conn.removeMute(options).then((res) => {
+        console.log(res)
+})
+      }
+      console.log(mem)
     }
+  },
+  components: {
+    Modal
   }
 }
 </script>
